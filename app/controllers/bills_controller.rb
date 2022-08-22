b5bf945e1b74dbf1b891d67d778a454c2b59c095
msalvi-perceptyx <msalvi@perceptyx.com>
2022-08-22 12:17:27 +0530

    create bill succesfully

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -1,70 +1,27 @@
 class BillsController < ApplicationController
-  before_action :set_bill, only: %i[ show edit update destroy ]
-
-  # GET /bills or /bills.json
   def index
     @bills = Bill.all
   end
 
-  # GET /bills/1 or /bills/1.json
-  def show
-  end
-
-  # GET /bills/new
   def new
+    @categories = Item.where(item_type: 'category')
     @bill = Bill.new
   end
 
-  # GET /bills/1/edit
-  def edit
-  end
-
-  # POST /bills or /bills.json
   def create
     @bill = Bill.new(bill_params)
 
-    respond_to do |format|
-      if @bill.save
-        format.html { redirect_to bill_url(@bill), notice: "Bill was successfully created." }
-        format.json { render :show, status: :created, location: @bill }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @bill.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # PATCH/PUT /bills/1 or /bills/1.json
-  def update
-    respond_to do |format|
-      if @bill.update(bill_params)
-        format.html { redirect_to bill_url(@bill), notice: "Bill was successfully updated." }
-        format.json { render :show, status: :ok, location: @bill }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @bill.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /bills/1 or /bills/1.json
-  def destroy
-    @bill.destroy
-
-    respond_to do |format|
-      format.html { redirect_to bills_url, notice: "Bill was successfully destroyed." }
-      format.json { head :no_content }
+    if @bill.save
+      redirect_to bills_path
+    else
+      @categories = Item.where(item_type: 'category')
+      render :new
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_bill
-      @bill = Bill.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
     def bill_params
-      params.require(:bill).permit(:name, :mobile_no, :price)
+      params.require(:bill).permit(:name, :father_name, :address, :mobile_no, :price, :item_id)
     end
 end
--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,2 +1,5 @@
 class Bill < ApplicationRecord
+  belongs_to :item
+  validates_presence_of :name, :father_name, :address, :price, :mobile_no
+
 end
